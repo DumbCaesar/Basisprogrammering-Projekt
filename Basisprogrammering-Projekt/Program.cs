91bb0aa8c72f7d3c7d0e9046d83cce4cfa88b45c
NicolasHO-eadania <niov6@student.eadania.dk>
2025-09-15 19:45:19 +0200

    Hangman tilføjet

--- Basisprogrammering-Projekt/Program.cs
+++ Basisprogrammering-Projekt/Program.cs
@@ -10,6 +10,64 @@ namespace Basisprogrammering_Projekt
         }
 
         // =====================================
+        //               Hangman
+        //                  
+        //           Author: Nicolas
+        // =====================================
+
+        static void Hangman()
+        {
+            string[] wordList = { "TIGER", "CRANES", "GLUE", "BICYCLE", "ELEPHANT", "CRASH", "FLUSH", "READING", "CONSTRUCTION" };
+            Random random = new Random();
+            int randomNumber = random.Next(wordList.Length);
+            string randomWord = wordList[randomNumber];
+            int randomWordLength = randomWord.Length;
+            int lives = 5;
+
+            string word = "";
+            for (int i = 0; i < randomWordLength; i++)
+            {
+                word += "-";
+            }
+
+            while (lives > 0 && word.Contains("-"))
+            {
+                Console.Clear();
+                Console.WriteLine(word);
+                Console.WriteLine("Lives left: " + lives);
+                Console.WriteLine("Guess a letter");
+                string input = Console.ReadLine().ToUpper();
+
+                char guess = Convert.ToChar(input);
+                bool correct = false;
+
+                for (int i = 0; i < randomWordLength; i++)
+                {
+                    if (randomWord[i] == guess)
+                    {
+                        word = word.Remove(i, 1).Insert(i, guess.ToString());
+                        correct = true;
+                    }
+                }
+                if (!correct)
+                {
+                    lives--;
+                }
+            }
+            if (word.Contains("-"))
+            {
+                Console.Clear();
+                Console.WriteLine("You Lose! The word was: " + randomWord);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You Win! The word was: " + randomWord);
+            }
+        }
+
+
+        // =====================================
         //         Sæt tal i rækkefølge
         //                  
         //           Author: Oliver
